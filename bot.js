1ed24df35c8c0084269b64ec495bdc7424b9e859
ALittleHalfHearted <33755588+ALittleHalfHearted@users.noreply.github.com>
2018-04-10 15:50:43 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,17 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+var d = new Date();
+const activated = (d.getMonth() + 1) + '/' + d.getDate() + '/' + d.getFullYear();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+	console.log(`Logged in as ${client.user.tag}!`);
+	console.log('Online at ' + activated);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
+client.on('message', message => {
+	if (message.content === 'ping') {
+		message.reply('Pong!');
+	}
 });
 
 // THIS  MUST  BE  THIS  WAY
