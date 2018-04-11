17431f19b50b2185fbd990d86b28b9ceee519e6c
ALittleHalfHearted <33755588+ALittleHalfHearted@users.noreply.github.com>
2018-04-11 09:57:57 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -67,7 +67,7 @@ client.on('message', message => {
 					}
 					if(args[i].indexOf('+' || '-' || '*' || '/') != -1){
 						val = args[i].substring(args[i].indexOf('d') + 1,args[i].indexOf('+' || '-' || '*' || '/'));
-						mod = calculate(args[i].substring(args[i].indexOf('+' || '-' || '*' || '/')));
+						mod = Modding(args[i].substring(args[i].indexOf('+' || '-' || '*' || '/')));
 					}
 					else{
 						args[i].substring(args[i].indexOf('d') + 1);
@@ -153,7 +153,7 @@ function Dice(check,num,val,mod,rpt){
 	return check;
 }
 
-function modding(calculate){
+function Modding(calculate){
 	if (isFinite(calculate.replace(/\=|\+|\-|\*|\/|\÷|\%|\(|\)|\,|\ |math.|pow|sqrt|round|floor|ceiling|ceil|pi|π|euler|absolute|abs|exp|logarithm|log|random|rand|rng/g,''))) {
 		calculate = calculate.replace(/ /g, "").replace(/÷/g, "/").replace(/power|pow/g, "Math.pow").replace(/sqrt|squareroot/g, "Math.sqrt").replace(/round/g, "Math.round").replace(/floor/g, "Math.floor").replace(/ceiling|ceil/g, "Math.ceil").replace(/pi|π/g, "Math.PI").replace(/euler/g, "Math.E").replace(/absolute|abs/g, "Math.abs").replace(/exp/g, "Math.exp").replace(/logarithm|log/g, "Math.log").replace(/random|rand|rng/g, "Math.random()");/*.replace(/acos|arccosine/g, "Math.acos").replace(/asin|arcsine/g, "Math.asin").replace(/atan|arctangent|atan1|arctangent1/g, "Math.atan").replace(/atan2|arctangent2/g, "Math.atan2").replace(/cos|cosine/g, "Math.cos").replace(/sin|sine/g, "Math.sin").replace(/tan|tangent/g, "Math.tan")*/;
 		if (calculate.replace(/[^%]/g, "").length > 0) {
