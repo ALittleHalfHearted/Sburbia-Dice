29ed40e97fcb37af0950e8d382f5836014363ff1
ALittleHalfHearted <33755588+ALittleHalfHearted@users.noreply.github.com>
2018-04-10 21:35:20 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,13 +15,70 @@ client.on('message', message => {
 		var cmd = args[0];
 		args = args.splice(1).toString().replace(/,/g,' ');
 		
+		var tier = 1;
+		var num = 1;
+		var bg = 0;
+		var t1 = 0;
+		var t2 = 0;
+		var t3 = 0;
+		var t4 = 0;
+		var t5 = 0;
+		var t6 = 0;
+		var t7 = 0;
+		var t8 = 0;
+		var t9 = 0;
+		var boon = 0;
+		var imps = 0;
+		var ogres = 0;
+		var basilisks = 0;
+		var liches = 0;
+		var giclopes = 0;
+		var titachnids = 0;
+		var archerons = 0;
+		var rooks = 0;
+		var grist = 0;
+		var health = 0;
+		var results = '';
+		
 		switch(cmd){
 			case 'ping':
 				message.reply('Pong!');
+			break;
+			//+imp [#] t[#]
+			case 'imp': case 'imps':
+				//5 exp, d10 boon, 2d20+10 bg, d20+5 t1, d5 t2
+				if(args.indexOf('t') != -1){
+					tier = args.substring(args.indexOf('t') + 1,args.indexOf('t') + 2);
+					args = args.replace(args.substring(args.indexOf('t'),args.indexOf('t') + 2),'');
+				}
+				args = args.replace(/ /g,'');
+				if(args != ''){
+					num = args;
+				}
+				switch(tier){
+					case '1':
+						boon = '\nboon: ' + Dice(boon,num,10);
+						bg = '\nbg: ' + Dice(bg,num * 2,20,10);
+						t1 = '\nt1: ' + Dice(t1,num,20,5);
+						t2 = '\nt2: ' + Dice(t2,num,5);
+						results = boon + bg + t1 + t2;
+					break;
+				}
+				BroadcastDrops(cmd,num,results);
 			break;
 		}
 	}
 });
 
-// THIS  MUST  BE  THIS  WAY
+function Dice(check,num,val,mod){
+	for(var i = 0;i < num;i++){
+		check = check + Math.floor(Math.random() * val) + 1;
+	}
+	return val + mod;
+}
+
+function BroadcastDrops(cmd,num,results){
+	message.reply('```For killing ' + num + ' ' + cmd + ', you got:' + results);
+}
+
 client.login(process.env.BOT_TOKEN);
