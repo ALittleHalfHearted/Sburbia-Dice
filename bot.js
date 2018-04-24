b613614f299f2d71b54c6bb8f5d7972b4b62278c
ALittleHalfHearted <33755588+ALittleHalfHearted@users.noreply.github.com>
2018-04-24 16:51:49 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -34,8 +34,8 @@ client.on("guildCreate", (guild) => {
 	var found = false;
 	var memers = guild.members.array();
 	for(var i = 0;i < memers.length && found == false ;i++){
-		let memei = memers[i].toString();
-		if(memei.indexOf(client.user) != -1){
+		let memei = memers[i];
+		if(memei.indexOf(client.user.id) != -1){
 			var member = memei;
 			found = true;
 		}
